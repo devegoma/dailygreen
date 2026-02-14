4d495e9cc0cc755817a02ee14b0fd2b8316c37e5
kani3camp <kani3camp@gmail.com>
2026-02-14 18:24:43 +0900

    Update link security attributes in Welcome component

--- web-app/app/welcome/welcome.tsx
+++ web-app/app/welcome/welcome.tsx
@@ -31,7 +31,7 @@ export function Welcome() {
                     className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
                     href={href}
                     target="_blank"
-                    rel="noreferrer"
+                    rel="noopener noreferrer"
                   >
                     {icon}
                     {text}
