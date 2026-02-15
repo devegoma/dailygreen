695f51c197f1ee73281946a783d867c6643c42c3
kani3camp <kani3camp@gmail.com>
2026-02-15 15:21:41 +0900

    fix(welcome): add comments to ignore linting for decorative SVG icons

--- web-app/app/welcome/welcome.tsx
+++ web-app/app/welcome/welcome.tsx
@@ -51,6 +51,7 @@ const resources = [
 		href: "https://reactrouter.com/docs",
 		text: "React Router Docs",
 		icon: (
+			// biome-ignore lint/a11y/noSvgWithoutTitle: decorative icon with aria-hidden; link text provides context
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="24"
@@ -72,6 +73,7 @@ const resources = [
 		href: "https://rmx.as/discord",
 		text: "Join Discord",
 		icon: (
+			// biome-ignore lint/a11y/noSvgWithoutTitle: decorative icon with aria-hidden; link text provides context
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="24"
